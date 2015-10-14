0f2ec1465e9b3b9e9a4d5e1eb2a97d41a7ace8df
alvaro-g <alvarogra91@gmail.com>
2015-10-14 23:52:06 +0200

    f2

--- first.py
+++ first.py
@@ -57,29 +57,35 @@ def construirVariablesVer(tauler,Y):
 
 def crearVariables(X,Y):
     '''Recorrent els taulers X,Y crea la llista de variables'''
-    contParaules=-1
-    contEspais=0
-    indice = 0
-    variablesReturn = []
+
+    contEspais=0 #conta els espais que té cada paraula
+    indice = 0 # fixa l'id d'una paraula per afegir-ho a la variable
+    variablesReturn = [] #llista on s'omplen les variables
+
+    #variables Horitzontals
     for x in range(0,X.shape[0]):
         for y in range(0,X.shape[1]):
+            #Troba al tauler un id no assignat, assigna indice per fixar aquest indice
             if (X[x,y] > 0 and X[x,y] != indice):
                 indice = X[x,y]
-                contParaules += 1
                 contEspais=1
+            #assigna a la variable l'index fixat
             elif (X[x,y] == indice):
                 contEspais+=1
                 if (contEspais > 1):
                     if (y==X.shape[0]-1 or X[x,y+1] == 0):
                         m = [indice, contEspais,-1,1]
                         variablesReturn.append(m)
+
+    #variables verticals
     contEspais=0
     for x in range(0,Y.shape[0]):
         for y in range(0,Y.shape[1]):
+            #Troba al tauler un id no assignat, assigna indice per fixar aquest indice
             if (Y[y,x] > 0 and Y[y,x] != indice):
                 indice = Y[y,x]
-                contParaules += 1
                 contEspais=1
+            #assigna a la variable l'index fixat
             elif (Y[y,x] == indice):
                 contEspais+=1
                 if (contEspais > 1):
@@ -88,41 +94,30 @@ def crearVariables(X,Y):
                         variablesReturn.append(m)
     return variablesReturn
 
-def contarParaules(X,Y):
-    contParaules=0
-    indice = 0
-    for x in range(0,X.shape[0]):
-        for y in range(0,X.shape[1]):
-            if (X[x,y] > 0 and X[x,y] != indice):
-                indice = X[x,y]
-                contParaules += 1
-    for x in range(0,Y.shape[0]):
-        for y in range(0,Y.shape[1]):
-            if (Y[y,x] > 0 and Y[y,x] != indice):
-                indice = Y[y,x]
-                contParaules += 1
-    return contParaules
-
 def construirRestriccions(X,Y):
     '''recorrer els taulers X,Y per saber on hi ha conflicte entre diferents paraules i retorna una llista amb aquestes restriccions'''
-    restriccions = []
-    contVer = {}
-    contHor = {}
+
+    restriccions = [] #llista on s'afegiran les restriccions
+    contVer = {} #diccionari per paraules horitzontals
+    contHor = {} #diccionari per paraules verticals
+
     for x in range (0, tauler.shape[0]):
         for y in range (0, tauler.shape[1]):
-            if (X[x][y] >0):
+            #comprova ID tauler horitzontals si hi ha paraula incrementa aquella posicio del diccionari
+            if (X[x][y] > 0):
                 if(X[x][y] not in contHor.keys()):
                     contHor[X[x][y]]=0
                 else:
                     contHor[X[x][y]]+=1
-            if(Y[x][y] >0):
+            #comprova ID tauler verticalss si hi ha paraula incrementa aquella posicio del diccionari
+            if(Y[x][y] > 0):
                 if(Y[x][y] not in contVer.keys()):
                     contVer[Y[x][y]]=0
                 else:
                     contVer[Y[x][y]]+=1
 
-            if(X[x][y] >0 and Y[x][y] >0):
-                #m = np.array([(X[x][y],contHor[X[x][y]],Y[x][y],contVer[Y[x][y]])], dtype=dtRestr)
+            #Si les posicions al diccionari son mes grans que 1 es que hi ha conflicte i l'afegeix a la llista de conflictes
+            if(X[x][y] > 0 and Y[x][y] > 0):
                 m = [X[x][y],contHor[X[x][y]],Y[x][y],contVer[Y[x][y]]]
                 restriccions.append(m)
     return restriccions
@@ -145,18 +140,18 @@ def SatisfaRestriccions(v, LVA, R, DA, D):
     else:
         for restriccio in R:
             if v[3]==1:  #horitzontal
-                if restriccio[0]==v[0]:
+                if restriccio[0]==v[0]: #coincideix la restricció amb la variable
                     for varAssig in LVA:
-                        if varAssig[3]==2:
-                            if varAssig[0]==restriccio[2]:
-                                if not D[v[1]][v[2]][restriccio[1]]== D[varAssig[1]][varAssig[2]][restriccio[3]]:
+                        if varAssig[3]==2: #comproba la restricció amb tota la llista de verticals de LVA
+                            if varAssig[0]==restriccio[2]: #si troba que hi ha restricció
+                                if not D[v[1]][v[2]][restriccio[1]]== D[varAssig[1]][varAssig[2]][restriccio[3]]:#comproba al diccionari la posicio de la lletra on hi ha restriccio
                                     return False
             else:    #vertical
-                if restriccio[2]==v[0]:
-                    for varAssig in LVA:
+                if restriccio[2]==v[0]:#coincideix la restricció amb la variable
+                    for varAssig in LVA:#comproba la restricció amb tota la llista de verticals de LVA
                         if varAssig[3]==1:
                             if varAssig[0]==restriccio[0]:
-                                if not D[v[1]][v[2]][restriccio[3]]== D[varAssig[1]][varAssig[2]][restriccio[1]]:
+                                if not D[v[1]][v[2]][restriccio[3]]== D[varAssig[1]][varAssig[2]][restriccio[1]]:#comproba al diccionari la posicio de la lletra on hi ha restriccio
                                     return False
         return True
 
@@ -167,11 +162,12 @@ def printaSolucio(taulerHor, taulerVer, llista,dicc):
         indexLletra = 0
         for y in range (0, taulerHor.shape[1]):
             if (int(taulerHor[x,y]) > 0):
+                #si troba un id busca a la llista de variables la variable amb aquest id i ho afegeix a solucio buscant-la al diccionari
                 for j in range(0, len(llista)):
                     if (llista[j][0] == int(taulerHor[x,y]) and llista[j][3]==1):
                         solucio[x,y]=chr(dicc[llista[j][1]][llista[j][2]][indexLletra])
                         indexLletra+=1
-                        #print (chr(dicc[llista[j][1]][llista[j][2]][y-llista[j][1]]))
+
             else:
                 indexLletra = 0
                 solucio[x,y]="#"
@@ -179,38 +175,40 @@ def printaSolucio(taulerHor, taulerVer, llista,dicc):
         indexLletra = 0
         for x in range (0, taulerVer.shape[1]):
             if (int(taulerVer[x,y]) > 0):
+                #si troba un id busca a la llista de variables la variable amb aquest id i ho afegeix a solucio buscant-la al diccionari
                 for j in range(0, len(llista)):
                     if (llista[j][0] == int(taulerVer[x,y]) and llista[j][3]==2):
                         solucio[x,y]=chr(dicc[llista[j][1]][llista[j][2]][indexLletra])
                         indexLletra+=1
-                        #print (chr(dicc[llista[j][1]][llista[j][2]][y-llista[j][1]]))
+
             else:
                 indexLletra = 0
-                #solucio[x,y]="!"
+
     return solucio
 
 def ActualitzarDominis(v, LVNA, R, DA, D):
     '''per cada DA comprova totes les restriccions amb LVNA per actualitzar cada domini propi de cada variable'''
     for restriccio in R:
         if v[3]==1: #horitzontal
-            if restriccio[0]==v[0]:
+            if restriccio[0]==v[0]:#comproba si la variable té cap restricció
                 for vna in LVNA:
                     if vna[3]==2:
-                        if vna[0]==restriccio[2]:
+                        if vna[0]==restriccio[2]:#comproba la restriccio amb tota la llista de variables no asignades horitzontals
                             domini = DA[(vna[0],vna[3])]
-                            #nouDomini = np.array([],dtype=np.int32)
                             nouDomini = []
+                            #crea un nou domini per afegir tots els index del domini vell que satisfan la restricció
                             for index in range(len(domini)):
                                 if D[v[1]][v[2]][restriccio[1]]== D[vna[1]][domini[index]][restriccio[3]]:
                                     nouDomini.append(domini[index])
                             DA[(vna[0],vna[3])] = nouDomini
         else:
-             if restriccio[2]==v[0]:
+             if restriccio[2]==v[0]:#comproba si la variable té cap restricció
                 for vna in LVNA:
-                    if vna[3]==1:
+                    if vna[3]==1:#comproba la restriccio amb tota la llista de variables no asignades verticals
                         if vna[0]==restriccio[0]:
                             domini = DA[(vna[0],vna[3])]
                             nouDomini = []
+                             #crea un nou domini per afegir tots els index del domini vell que satisfan la restricció
                             for index in range(len(domini)):
                                 if D[v[1]][v[2]][restriccio[3]]== D[vna[1]][domini[index]][restriccio[1]]:
                                     nouDomini.append(domini[index])
@@ -256,11 +254,12 @@ def Backtracking(LVA, LVNA, R, DA, D):
 
 if __name__ == "__main__":
     t1=0
-    for i in range(0,1):
+    vegades = 1
+    for i in range(0,vegades):
         t0 = time.clock()
 
-        fitxer_dic = "diccionari_CB.txt"
-        fitxer_tau = "crossword_CB.txt"
+        fitxer_dic = "diccionari_A.txt"
+        fitxer_tau = "crossword_A.txt"
         diccionari = np.genfromtxt(fitxer_dic,dtype='S16')
         tauler = np.loadtxt(fitxer_tau, dtype='S16', comments='!')
         tauler.tostring()
@@ -295,5 +294,5 @@ if __name__ == "__main__":
                 print ("\n")
         print ("-----\t-------\t-------\t-------\t-------")
         t1 += (time.clock() - t0)
-    final = t1/1
+    final = t1/vegades
     print ("%.2f sec" % final)
